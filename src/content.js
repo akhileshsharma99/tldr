835ff8fcac2549752e2a19d2afea8e14c54ca502
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2023-06-08 23:27:49 -0400

    kewl

---
+++ src/content.js
@@ -0,0 +1,17 @@
+// const { Readability } = require("@mozilla/readability");
+
+// // chrome.runtime.onMessage.addListener(
+// //     function (request, sender, sendResponse) {
+// //         if (request.contentScriptQuery == "parseArticle") {
+// //             let reader = new Readability(document.cloneNode(true));
+// //             let article = reader.parse();
+// //             console.log(article);
+// //             sendResponse(article);
+// //             return true;  // Will respond asynchronously.
+// //         }
+// //     }
+// // );
+
+// let reader = new Readability(document.cloneNode(true));
+// let article = reader.parse();
+// console.log(article);
